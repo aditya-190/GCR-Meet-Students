4569050db96b52e769e2e5b9d82038b36b4718aa
Aditya Bhardwaj <ab2225@srmist.edu.in>
2021-10-07 15:14:29 +0530

    Small but Important Changes.

--- main.js
+++ main.js
@@ -31,3 +31,8 @@ $(document.getElementById("googleMeet")).click(function() {
 
 $(document.getElementById("googleClassroom")).click(function() {
 });
+
+function ValidURL(str) {
+    const regex = /(http|https):\/\/(\w+:?\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%\-\/]))?/;
+    return regex.test(str);
+}
