dc7ccc352fb5c233f3fa26c71acc812a1386cec9
Aditya Bhardwaj <ab2225@srmist.edu.in>
2021-10-07 16:24:03 +0530

    Input Validity Checker Added.

--- main.js
+++ main.js
@@ -1,38 +1,83 @@
 // Code Handling DropBox Select Options - Starts.
 
-$('.selectedContainer').click(function(e) {
+$('.selectedContainer').click(function (e) {
     e.preventDefault();
     e.stopPropagation();
     $(this).toggleClass('expanded');
-    $('#'+$(e.target).attr('for')).prop('checked',true);
+    $('#' + $(e.target).attr('for')).prop('checked', true);
 });
-$(document).click(function() {
+$(document).click(function () {
     $('.selectedContainer').removeClass('expanded');
 });
 
 // Code Handling DropBox Select Options - Ends.
 
-$('.minusButton').click(function() {
-    const snackbar = document.getElementById("snackbar");
-    snackbar.className = "show";
-    snackbar.innerText = "Removed Successfully."
-    setTimeout(function(){ snackbar.className = snackbar.className.replace("show", ""); }, 3000);
-});
-
-$('.plusButton').click(function() {
-    const snackbar = document.getElementById("snackbar");
-    snackbar.className = "show";
-    snackbar.innerText = "Added Successfully."
-    setTimeout(function(){ snackbar.className = snackbar.className.replace("show", ""); }, 3000);
-});
-
-$(document.getElementById("googleMeet")).click(function() {
-});
-
-$(document.getElementById("googleClassroom")).click(function() {
-});
-
 function ValidURL(str) {
     const regex = /(http|https):\/\/(\w+:?\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%\-\/]))?/;
     return regex.test(str);
 }
+
+$('.minusButton').click(function () {
+    const snackbar = document.getElementById("snackbar");
+    snackbar.className = "show";
+    snackbar.innerText = "Removed Successfully."
+    setTimeout(function () {
+        snackbar.className = snackbar.className.replace("show", "");
+    }, 3000);
+});
+
+$('.plusButton').click(function () {
+    const classNameExtract = document.getElementById("className").value;
+    const meetLinkExtract = document.getElementById("meetLink").value;
+    const classroomLinkExtract = document.getElementById("classroomLink").value;
+
+    if (!ValidURL(meetLinkExtract)) {
+        document.querySelector("#meetContainer").style.border = "1px solid #FF5C58";
+        setTimeout(() => {
+            document.querySelector("#meetContainer").style.border = "";
+        }, 2000);
+
+        return;
+    }
+
+    if (classNameExtract === "") {
+        document.querySelector("#classNameContainer").style.border = "1px solid #FF5C58";
+        setTimeout(() => {
+            document.querySelector("#classNameContainer").style.border = "";
+        }, 2000);
+
+        return;
+    }
+
+    if (!ValidURL(classroomLinkExtract)) {
+        document.querySelector("#classRoomContainer").style.border = "1px solid #FF5C58";
+        setTimeout(() => {
+            document.querySelector("#classRoomContainer").style.border = "";
+        }, 2000);
+
+        return;
+    }
+
+    let singleContent = {
+        "className": classNameExtract,
+        "meetLink": meetLinkExtract,
+        "classroomLink": classroomLinkExtract,
+    }
+
+    document.getElementById("className").value = "";
+    document.getElementById("meetLink").value = "";
+    document.getElementById("classroomLink").value = "";
+
+    const snackbar = document.getElementById("snackbar");
+    snackbar.className = "show";
+    snackbar.innerText = "Added Successfully."
+    setTimeout(function () {
+        snackbar.className = snackbar.className.replace("show", "");
+    }, 3000);
+});
+
+$(document.getElementById("googleMeet")).click(function () {
+});
+
+$(document.getElementById("googleClassroom")).click(function () {
+});
